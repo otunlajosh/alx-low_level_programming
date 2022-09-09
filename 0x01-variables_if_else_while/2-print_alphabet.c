6ecfc7583c3ba0caa24e8e91611035cf8aa5d8b9
Otunlajosh <otunlajosh@gmail.com>
2022-09-09 11:28:53 -0700

    Task 2 pring a-z in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print if the number is positive, negative or zero
+ * Description: Using the main function
+ * this program prints "Programming is positive, negative or zero"
+ * Return 0.
+ */
+	int main(void)
+{
+	char ch;
+	for (ch ='a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+
+
+}
