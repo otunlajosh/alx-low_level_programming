f6184da48d700944b3fd8dd110d0a9a963dce6d6
Otunlajosh <otunlajosh@gmail.com>
2022-09-12 13:18:50 -0700

    Task 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 * main - print the string in the put function
 *
 * Description: using the main fxn
-* Return 0
+* Return (0).
 */
-int main(void)                                                                                                  
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
