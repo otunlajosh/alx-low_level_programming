0d53052d70d5d7a903f6969a29cb0712a485bbe6
Otunlajosh <otunlajosh@gmail.com>
2022-09-12 04:12:36 -0700

    Task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	    char c;
-		 for (c = 'a'; c <= 'z'; c++)
-		 {
-			 putchar(c);
-		 }
-		for (c = 'A'; c <= 'Z'; c++)
-		 {
-			putchar(c);
-		}
+	 char c;
+	 for (c = 'a'; c <= 'z'; c++)
+	 {
+		putchar(c);
+	 }
+	for (c = 'A'; c <= 'Z'; c++)
+	 {
+		putchar(c);
+	}
 		 putchar('\n');
 		return (0);
 }
