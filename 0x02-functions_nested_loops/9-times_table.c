029eedd31c5a5dbe4a8e6e36078c20ce676b51a0
Otunlajosh <otunlajosh@gmail.com>
2022-09-14 15:08:07 -0700

    Task9, timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 	int mult;
 	int prod;
 
-	for (num = 0; num <= 9; num ++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
 
@@ -18,12 +18,12 @@ void times_table(void)
 			_putchar(' ');
 
 			prod = num * mult;
-			
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-			
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
