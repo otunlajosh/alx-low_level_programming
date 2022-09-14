536cd5cb7944299a5d2356716ed77ef519ffdf80
Otunlajosh <otunlajosh@gmail.com>
2022-09-14 07:04:23 -0700

    Task 6 Absolute Value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value 
+ * @c: is the int that will use for the argu
+ * Return: 0
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+	return (c);
+	}
+	else
+	return (c * -1);
+}
