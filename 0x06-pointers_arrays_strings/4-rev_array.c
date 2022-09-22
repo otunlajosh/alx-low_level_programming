764a7512c468207ef04856b91f89b8abcc021d49
Otunlajosh <otunlajosh@gmail.com>
2022-09-22 08:49:44 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 
 void reverse_array(int *a, int n)
 {
-	        int tmp, index;
+	int tmp, index;
 
-		for (index = n - 1; index >= n / 2; index--)
-		{
-			tmp = a[n - 1 - index];
-			a[n - 1 - index] = a[index];
-			a[index] = tmp;
-		}
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
 }
