3927ae4f6620d962949c32530639d1d763963a9c
Otunlajosh <otunlajosh@gmail.com>
2022-09-26 06:16:22 -0700

    Task 0, memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: A pointer to the memory area to be filled.
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
- * description _memset: over there
+ * description _memset: over there.
  *
  * Return: A pointer to the filled memory area @s
  */
@@ -18,6 +18,6 @@ void *_memset(void *s, int c, size_t n)
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
-	
+
 	return (memory);
 }
