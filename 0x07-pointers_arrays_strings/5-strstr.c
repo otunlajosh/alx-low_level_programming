6caaedc82bb959a9b723425960989424ecabc1f2
Otunlajosh <otunlajosh@gmail.com>
2022-09-27 10:48:40 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * strstr - Locates a substring.
+ * _strstr - Locates a substring.
  * @haystack: The string to be searched.
  * @needle: The substring to be located.
  *
- * Return: If the substring is located - a pointer to the beginning
+ * Return: If the substring is located - a pointer to the begin
  * of the located substring.
  * If the substring is not located - NULL.
  */
