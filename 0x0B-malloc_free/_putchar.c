ef1cf17de40bbbf166dbea6f7e7feee9b650668c
Otunlajosh <otunlajosh@gmail.com>
2022-10-04 13:12:45 -0700

    Task 0

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character to stdout
+ * @c - the character input
+ * 
+ * Return: on sucess 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	rerturn (write(1, &c, 1));
+}
