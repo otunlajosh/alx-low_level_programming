5a4e216300095755f649b2d3324202b3f746ac17
Otunlajosh <otunlajosh@gmail.com>
2022-10-05 12:54:27 -0700

    Task 0, cr5eate array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,39 +1,28 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * create_array - array for prints a string
- * @size: number elements array
- * @c: char
- * Return: pointer
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ * @size: Size of the array
+ * @c: Character to insert
+ * Return: NULL if size is zero or if it fails,
+ * pointer to array if everything is normal.
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+	char *array;
+	unsigned int index;
 
-if (size == 0)
-{
-	return (NULL);
-}
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
 
-/* Define values with malloc*/
-buffer = (char *) malloc(size * sizeof(c));
+	if (array == NULL)
+		return (NULL);
 
-if (buffer == 0)
-{
-	return (NULL);
-}
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-else
-{
-	position = 0;
-	while (position < size) /*while for array*/
-	{
-		*(buffer + position) = c;
-		position++;
-	}
-
-	return (buffer);
+	return (array);
 }
--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -11,5 +11,5 @@
 
 int _putchar(char c)
 {
-	rerturn (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
