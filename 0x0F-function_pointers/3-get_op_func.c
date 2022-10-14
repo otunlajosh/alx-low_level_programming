565244e3d938c5e609f0ff2999dbe433c618ad6d
Otunlajosh <otunlajosh@gmail.com>
2022-10-14 11:39:56 -0700

    Task 3- get op function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function pointer that select the correct function to perform
+ * @s: operator given by user
+ * Return: pointer to function
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	opt_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
